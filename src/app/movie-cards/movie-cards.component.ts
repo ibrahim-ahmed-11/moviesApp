533f78091cd9b5c3517ef478e220b56400a986f3
Ibrahim Ahmed <ibrahim-ahmed-11@users.noreply.github.com>
2019-07-18 21:36:21 +0200

    added pagination to movies list

--- src/app/movie-cards/movie-cards.component.ts
+++ src/app/movie-cards/movie-cards.component.ts
@@ -15,16 +15,32 @@ export class MovieCardsComponent implements OnInit {
   constructor(private service: MoviesService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params =>{
+
+    this.route.paramMap.subscribe(params => {
       this.pageID = +params.get('page');
+      this.getPosts();
+      this.scrollToTopPage();
     });
 
+  }
+
+  getPosts() {
     this.service.getPosts(20, this.pageID).subscribe(response => {
       this.moviesData = response["data"]["movies"];
-    },
-      error => {
-        console.log(error);
-      });
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  scrollToTopPage() {
+    let scrollToTop = window.setInterval(() => {
+      let pos = window.pageYOffset;
+      if (pos > 0) {
+        window.scrollTo(0, pos - 20); // how far to scroll on each step
+      } else {
+        window.clearInterval(scrollToTop);
+      }
+    }, 5);
   }
 
 }
